245969c78763baacc2a501824e286f82ec689429
Dauda Hammed <dauda.oluwatoyin.hammed@gmail.com>
2020-07-16 01:01:32 +0100

    final commit

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchReader_1 = require("./MatchReader");
-var firstMatchReader = new MatchReader_1.MatchReader('football.csv');
-firstMatchReader.read();
-var output = firstMatchReader.winsAnalysis('Newcastle');
-firstMatchReader.printHTML_Report(output);
